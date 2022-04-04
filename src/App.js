9d967f53a206f326475d94fad9825b0d1477dfc6
zereentabassum <islamms262@gmail.com>
2022-04-04 16:27:55 +0600

    router added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Components/About/About';
+import Blogs from './Components/Blogs/Blogs';
+import Dashboard from './Components/Dashboard/Dashboard';
 import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
+import Review from './Components/Review/Review';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>} ></Route>
+        <Route path='/home' element={<Home></Home>} ></Route>
+        <Route path='/review' element={<Review></Review>} ></Route>
+        <Route path='/dashboard' element={<Dashboard></Dashboard>} ></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>} ></Route>
+        <Route path='/about' element={<About></About>} ></Route>
+      </Routes>
+
+     
      <h1 className='text-5xl text-teal-600'>Good Luck ;)</h1>
     </div>
   );
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>its home</h1>
+        </div>
+    );
+};
+
+export default Home;
