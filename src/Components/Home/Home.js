58de966f332903071e90fdb39c2ca7871dc95f2c
zereentabassum <islamms262@gmail.com>
2022-04-04 17:38:59 +0600

    camera photo section added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,8 +4,14 @@ import './Home.css';
 
 const Home = () => {
     return (
-        <div>
-            <h1>its home</h1>
+        <div className='flex'>
+            <div className='mt-8 pt-6'> 
+                <h1 className='text-8xl text-emerald-500 pb-8'>Clear Pixel Camera</h1>
+                <p text-2xl>This is about the best digital cameras from top brands. We will allow you to analysis the top branded digital cameras.</p>
+            </div>
+            <div className=''>
+                <img src="https://www.pngmart.com/files/6/DSLR-Camera-Transparent-Background.png" alt="" />
+            </div>
         </div>
     );
 };
